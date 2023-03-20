c4aad26928422952f3b0faca97aad6515a19d6a4
Marcel Rubin Perez <marcelrubin@MacBook-Pro-de-Marcel.local>
2023-03-20 23:29:15 +0100

    Update2

---
+++ exercice.py
@@ -0,0 +1,7 @@
+def loop_over_list():
+    # Write your code here
+    my_list=['Altuve', 'Bregman', 'Correa', 'Gattis','Brady']
+    for list in my_list:
+        print(list)
+    
+    return my_list
